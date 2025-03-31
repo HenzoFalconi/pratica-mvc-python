4a99d43febd5b133b45748fd41efe4407457ce1d
HenzoFalconi <henzofalconi@gmail.com>
2025-03-31 14:38:04 -0300

    cmt que apaga e cria

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
+from model.tarefa import Tarefa
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("agenda.html", title='Adicionar Tarefa')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        titulo = request.form['titulo']
+        data_conclusao = request.form['data_conclusao']
+        tarefa = Tarefa(titulo=titulo, data_conclusao=data_conclusao)
+        tarefa.salvarTarefa()
+        return redirect(url_for('index'))
+
+    tarefas = Tarefa.listarTarefas()
+    return render_template("index.html", tarefas=tarefas, title='Minhas Tarefas')
+
+@app.route('/delete/<int:idtarefa>')
+def delete(idtarefa):
+    tarefa = Tarefa(id=idtarefa)  # Passando o parâmetro correto 'id' para o construtor
+    tarefa.apagarTarefa()  # Chama o método para apagar a tarefa
+    return redirect(url_for('index'))
+
--- model/tarefa.py
+++ model/tarefa.py
@@ -1,41 +1,38 @@
-from database import Database
+from model.database import Database
 
 class Tarefa:
-    def __init__(self, id, titulo, data_conclusão):
+    def __init__(self, id=None, titulo=None, data_conclusao=None):
         self.id = id
         self.titulo = titulo
-        self.data_conclusão = data_conclusão
-        pass
+        self.data_conclusao = data_conclusao
 
     def salvarTarefa(self):
         """Salva uma nova tarefa no banco de dados."""
         db = Database()
         db.conectar()
 
-        sql = 'INSERT INTO tarefa (titulo, data_conclusão) VALUES (%s, %s)'
-        params = (self.titulo, self.data_conclusão)
+        sql = 'INSERT INTO tarefa (titulo, data_conclusao) VALUES (%s, %s)'
+        params = (self.titulo, self.data_conclusao)
         db.executar(sql, params)
         db.desconectar()
-        
-    def listarTarefas(self):
+
+    @staticmethod
+    def listarTarefas():
         """Retorna uma lista de tarefas cadastradas."""
         db = Database()
         db.conectar()
 
-        sql = 'SELECT id, titulo, data_conclusão FROM tarefa'  
+        sql = 'SELECT id, titulo, data_conclusao FROM tarefa'
         tarefas = db.consultar(sql)
         db.desconectar()
         return tarefas if tarefas else []
-    
+
     def apagarTarefa(self):
-        """Apaga todas as tarefas cadastrada no banco de dados."""
+        """Apaga uma tarefa cadastrada no banco de dados."""
         db = Database()
         db.conectar()
 
         sql = 'DELETE FROM tarefa WHERE id = %s'
-        params = (self.id,) # Precisa passar como tupla?
+        params = (self.id,)  # Corrigir como tupla
         db.executar(sql, params)
         db.desconectar()
-
-tarefa = Tarefa(4, 'Teste de Tarefa', None)
-tarefa.apagarTarefa()
